4f84fcc3bd430cab1df0d92ae00199b2f3712375
Maya <mayasidorova@yahoo.com>
2021-03-11 13:59:53 +0800

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+    str = str.split('');
+    let bi;
+    for (let i = 0; i < str.length; i++){
+      stack.push(str[i]);
+      for (let j = 0; j < bracketsConfig.length; j++) {
+        if (bracketsConfig[j].includes(str[i])) {
+          bi = j;
+        }
+      }
+      if (stack[stack.length - 1] == bracketsConfig[bi][1] && stack[stack.length - 2] == bracketsConfig[bi][0]){
+        stack.pop();
+        stack.pop();
+        }        
+      }      
+  if (stack.length == 0) return true;
+  return false;
 }
